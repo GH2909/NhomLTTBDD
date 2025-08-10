fa3a8c7052ce4cc15ab4a87deff2a13d4e616249
huydg2005 <youngduonghuy@gmail.com>
2025-08-10 12:52:35 +0700

    Commit các thay đổi mới trên nhánh dhuy

--- app/src/main/java/com/example/weborderingfood/Constants.java
+++ app/src/main/java/com/example/weborderingfood/Constants.java
@@ -1,9 +1,6 @@
 package com.example.weborderingfood;
 
 public class Constants {
-        public static final String BASE_URL = "http://10.11.187.252/ltweb/BTL/Webcoding-learning/";
-        public static final String LOGIN_URL = BASE_URL + "login.php";
-        public static final String MENU_URL = BASE_URL + "menu.php";
-        public static final String INDEX_URL = BASE_URL + "index.php";
+        public static final String BASE_URL = "http://192.168.1.172/ltweb/BTL/Webcoding-learning/";
     }
 
